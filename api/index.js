4c7ce4519cc3e7c8e0e7c52671fca2af9df0a4a1
Kunalchaudhary1716 <kunalchaudhary1716@gmail.com>
2024-12-11 21:38:15 +0530

    create user model

--- api/index.js
+++ api/index.js
@@ -1,5 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(`mongodb://127.0.0.1:27017/mern-estate`).then(()=>{
+    console.log("Database connected");
+}).catch ((err) => {
+    console.log(err);
+}); 
+ 
 const app = express();
 
 app.listen(3000, ()=>{
