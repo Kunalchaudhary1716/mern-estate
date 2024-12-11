5fcaf362d7559de9c5aa6e15f81cfc1d9c49a883
Kunalchaudhary1716 <kunalchaudhary1716@gmail.com>
2024-12-11 22:03:53 +0530

    create test api route

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
-
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js'
 dotenv.config();
 
 mongoose.connect(`mongodb://127.0.0.1:27017/mern-estate`).then(()=>{
@@ -15,3 +15,11 @@ const app = express();
 app.listen(3000, ()=>{
     console.log("server is running on Port 3000 ");
 });
+
+app.get('/test',(req,res)=>{
+    res.json({
+        message:'Hello World'
+    });
+});
+
+app.use("/api/user", userRouter);
