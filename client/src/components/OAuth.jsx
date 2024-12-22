41cf330fd981869f991060656c6afe8cb2819261
Kunalchaudhary1716 <kunalchaudhary1716@gmail.com>
2024-12-22 15:57:25 +0530

    create and add OAuth Component

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from 'react';
+import {getAuth, GoogleAuthProvider, signInWithPopup} from 'firebase/auth';
+import { app } from '../firebase';
+import { useDispatch } from 'react-redux';
+import { signInSuccess } from '../redux/user/userSlice';
+import { useNavigate } from 'react-router-dom';
 
-const OAuth = () => {
+export default function OAuth() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const handleGoogleClick = async() =>{ 
+    try{
+      const provider = new GoogleAuthProvider()
+      const auth = getAuth(app)
+
+      const result = await signInWithPopup(auth, provider)
+
+      const res = awaitfetch('/api/auth/google',{
+       method : 'POST',
+       header : {
+        'Content-Type': 'application/json',
+       },
+       body: JSON.stringify({name: result.name.user.displayName, email: result.user.email, photo: result.user.photoURL}),
+      })
+      const data = await res.json()
+      dispatch(signInSuccess(data));
+      navigate('/');
+    } catch(error){
+      console.log('Could not sign with google', error);
+    }
+  }
   return (
-    <div>OAuth</div>
+    <button onClick={handleGoogleClick} type='button' className='bg-red-700 text-white p-3 rounded-lg uppercase hover:scale-90'>Continue with Google</button>
   )
 }
-
-export default OAuth
